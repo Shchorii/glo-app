63fcc5b87d6372dfe5d2338a59012a77d463622c
Shchorii <85485947+Shchorii@users.noreply.github.com>
2026-09-09 09:51:01 +0300

    fix: add CARTO API key to basemap tile requests to remove watermark

--- src/components/BookMap.tsx
+++ src/components/BookMap.tsx
@@ -118,12 +118,15 @@ export default function BookMap({
       });
       mapRef.current = map;
 
-      L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: "abcd",
-        maxZoom: 19,
-      }).addTo(map);
+      L.tileLayer(
+        `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${process.env.NEXT_PUBLIC_CARTO_API_KEY}`,
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          subdomains: "abcd",
+          maxZoom: 19,
+        }
+      ).addTo(map);
 
       screens.forEach((s) => {
         const marker = L.marker([s.lat, s.lng], { icon: iconFor(L, false) }).addTo(map);
--- src/components/CampaignMap.tsx
+++ src/components/CampaignMap.tsx
@@ -42,7 +42,7 @@ export default function CampaignMap({ screens }: { screens: MapScreen[] }) {
       }
 
       L.tileLayer(
-        "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+        `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${process.env.NEXT_PUBLIC_CARTO_API_KEY}`,
         {
           attribution:
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
